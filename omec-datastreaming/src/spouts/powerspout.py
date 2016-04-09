234c1f7e7d2a64c21d780e5465c4469d09a0b062
Jackson Isack <jacksonisack@gmail.com>
2016-04-09 12:41:23 +0200

    spout initialization cleaning

--- omec-datastreaming/src/spouts/powerspout.py
+++ omec-datastreaming/src/spouts/powerspout.py
@@ -11,8 +11,7 @@ from streamparse.spout import Spout
 class PowerSpout(Spout):
 
     def initialize(self, stormconf, context):
-        self.words = itertools.cycle(['dog', 'cat',
-                                    'zebra', 'elephant'])
+        self.sleep_time = 1 
 
     def next_tuple(self):
         data= requests.get('http://127.0.0.1:8080').content
@@ -26,4 +25,4 @@ class PowerSpout(Spout):
                 email = d["email"]
                 threshold = d["threshold"]
                 self.emit([socket,power,email,threshold])
-        time.sleep(1)
+        time.sleep(self.sleep_time)
